3a1f77cf965e3bd117c0ffed160248371ae14d1a
AnuragSharma5893 <anuragsharma58693@gmail.com>
2026-04-23 02:23:51 +0530

    changese

--- src/app/guestbook/loading.tsx
+++
@@ -1,9 +0,0 @@
-// import { Flex, Spinner } from "@once-ui-system/core";
-
-// export default function Loading() {
-//     return (
-//         <Flex fillWidth paddingY="xl" horizontal="center">
-//             <Spinner />
-//         </Flex>
-//     );
-// }
